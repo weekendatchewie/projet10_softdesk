21259f5d41b9f765d7ee74305ae47a09901168de
Christopher P <christopher.poncelet51@gmail.com>
2022-06-30 16:11:33 +0200

    create model project

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -7,3 +8,22 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"Utilisateur : {self.username}"
+
+
+class Project(models.Model):
+    BACKEND = 'BACKEND'
+    FRONTEND = 'FRONTEND'
+    IOS = 'IOS'
+    ANDROID = 'ANDROID'
+
+    TYPE_CHOICES = (
+        (BACKEND, 'back-end'),
+        (FRONTEND, 'front-end'),
+        (IOS, 'ios'),
+        (ANDROID, 'android'),
+    )
+
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=2048, blank=True)
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
